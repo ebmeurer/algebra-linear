58418a758befd0e201f98e8717a83535010ab0c2
Eduardo Meurer <ebmeurer@unisc.br>
2019-07-08 19:57:28 -0300

    Adicionado while true no metodo principal

--- app_base.py
+++ app_base.py
@@ -7,10 +7,10 @@ funcoes = []
 funcoes += prova1.getFuncoes()
 funcoes += prova2.getFuncoes()
 funcoes += prova3.getFuncoes()
-
-print("Escolha o metodo a ser calculado")
-for i in range(len(funcoes)):
-    print(str(i+1) + "- "+ str(funcoes[i][0]))
-choice = int(input())-1
-print(eval(funcoes[choice][1]))
-
+while True:
+    print("Escolha o metodo a ser calculado")
+    for i in range(len(funcoes)):
+        print(str(i+1) + "- "+ str(funcoes[i][0]))
+    choice = int(input())-1
+    print(eval(funcoes[choice][1]))
+    print("")
